989104264a291572051d97ecd6e14bb2b91c93ac
Jeehan Hasan <jehan.hasan.bd@gmail.com>
2026-05-17 17:14:04 +0600

    shortest path find

--- 2_Algorithm/6_Graph/168_Shortest_path_undirected_graph.cpp
+++ 2_Algorithm/6_Graph/168_Shortest_path_undirected_graph.cpp
@@ -1,3 +1,55 @@
-//
-// Created by jehan on 5/17/2026.
-//
+#include <iostream>
+#include "algorithm"
+#include "Graph.h"
+
+using namespace std;
+
+vector<int> shortestPathUndirectedGraph(unordered_map<int,list<int>> &adjList, int source, int dest){
+    unordered_map<int,bool> visited;
+    unordered_map<int,int> parent;
+
+    visited[source] = true;
+    parent[source] = -1;
+    queue<int> q;
+    q.push(source);
+
+    while (!q.empty()){
+        int frontNode = q.front();
+        q.pop();
+
+        for (auto neighbor: adjList[frontNode]){
+            if (!visited[neighbor]){
+                q.push(neighbor);
+                visited[neighbor] = true;
+                parent[neighbor] = frontNode;
+            }
+        }
+    }
+
+    vector<int> visitedSeq;
+    visitedSeq.push_back(dest);
+
+    int prevNode = dest;
+    while (prevNode != source){
+        prevNode = parent[prevNode];
+        visitedSeq.push_back(prevNode);
+    }
+    reverse(visitedSeq.begin(), visitedSeq.end());
+    return visitedSeq;
+
+}
+
+int main()
+{
+    Graph g;
+    g.addEdge(1,2,false);
+    g.addEdge(2,3,false);
+    g.addEdge(2,4,false);
+    g.addEdge(3,4,false);
+    g.addEdge(3,7,false);
+    g.addEdge(7,6,false);
+    g.addEdge(5,6,false);
+    g.addEdge(4,5,false);
+    vector<int> visitedSeq = shortestPathUndirectedGraph(g.adjList,1,5);
+    printSeq(visitedSeq);
+}
---
+++ brach.md
@@ -0,0 +1,68 @@
+# Clone repository (optional)
+git clone <repository_url>
+
+# Go to project folder
+cd project_name
+
+# Check current branch
+```
+git branch
+```
+
+# Make sure you are on main
+```
+git checkout main
+```
+
+# Create new branch and switch to it
+```
+git checkout -b feature_branch
+```
+
+# Add or edit files
+# (do your coding here)
+
+# Check changed files
+```
+git status
+```
+
+# Add files
+```
+git add .
+```
+
+# Commit changes
+```
+git commit -m "Added new feature"
+```
+
+# Push new branch to GitHub
+```
+git push origin feature_branch
+```
+# Optional: delete branch locally
+```
+git branch -d feature_branch
+```
+
+# Switch back to main
+```
+git checkout main
+```
+
+# Pull latest main changes
+```
+git pull origin main
+```
+
+# Merge feature branch into main
+```
+git merge feature_branch
+```
+
+# Push updated main branch
+```
+git push origin main
+```
+
